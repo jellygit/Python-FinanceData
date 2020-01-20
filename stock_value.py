acc3c2a7bc9e1deafe4efc95de9a8cc5aafe81c9
JellyPo <jellypo@gmail.com>
2020-01-20 14:51:25 +0900

    modify stock_value.py, check table and multi argument

--- stock_value.py
+++ stock_value.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
-"""
-python get_stock_value.py 종목코드
-적으면 표준 출력으로 결과 나옴.
-"""
+#!/bin/env python
 import os
 import sys
 import datetime as dt
 import pandas as pd
 import sqlite3
+import matplotlib.ticker as mtick
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+
+PNG_FOLDER = "png/"
+if not os.path.isdir(PNG_FOLDER):
+    os.mkdir(PNG_FOLDER)
+
+# 폰트 설정
+mpl.rc('font', family='NanumGothic')
+# 유니코드에서  음수 부호설정
+mpl.rc('axes', unicode_minus=False)
 
 # 수수료 계산 함수.
 # 수수료는 매수/매도 시, Tax on sale. KOSPI 0.25, KOSDAQ 0.25
@@ -31,7 +40,7 @@ def create_connection(db_file):
         return conn
     except Error as e:
         print(e)
- 
+
     return conn
 
 def create_table(conn, create_table_sql):
@@ -49,14 +58,15 @@ def create_table(conn, create_table_sql):
 CONN = create_connection('./db/finance.db')
 BT = create_connection('./db/backtest.db')
 
-# SYM = '005930'
 COUNTRY = "KR"
-SYM = sys.argv[1]
+"""
+if len(sys.argv[1]) > 1:
+    SYM = sys.argv[1]
+else:
+    SYM = '005930'
 SYM = str(SYM)
 # COUNTRY = sys.argv[2]
-
-# SYM 변수가 finance.db에 없으면 에러 발생..
-DF = pd.read_sql('select * from \"' + SYM + '\"', con = CONN, index_col="Date")
+"""
 
 # Create Backtest Table.
 # if exist, read latest record.
@@ -65,7 +75,7 @@ def checkTableExists(dbcon, tablename):
     dbcur.execute("""
         SELECT COUNT(*)
         FROM sqlite_master
-        WHERE type = '{0}'
+        WHERE type = 'table' and name ='{0}'
         """.format(tablename.replace('\'', '\'\'')))
     if dbcur.fetchone()[0] == 1:
         dbcur.close()
@@ -76,46 +86,65 @@ def checkTableExists(dbcon, tablename):
     dbcur.close()
     return False
 
-if (checkTableExists(BT, SYM)):
-    print("Table is exist")
-else:
-    print("Table is not exist, Create Table " + SYM)
-    create_table(BT, SYM)
+def calcValuation(SYM):
+    # SYM 변수가 finance.db에 없으면 에러 발생..
+    DF = pd.read_sql('select * from \"' + SYM + '\"', con = CONN, index_col="Date")
 
-# 매월 투입금
-BUDGET = 500000
-# 거스름돈
-WALLET = 0
-# 보유주식 수
-STOCK = 0
-# 평가액
-PRINCIPAL = 0
+    # 매월 투입금
+    BUDGET = 500000
+    # 거스름돈
+    WALLET = 0
+    # 보유주식 수
+    STOCK = 0
+    # 평가액
+    PRINCIPAL = 0
 
-"""
-for year in range(2019, 2021):
-    for month in range(1, 13):
-        for day in range(1, 28):
-            date = dt.datetime(year, month, day, 0, 0, 0)
-            date = date.__format__("%Y-%m-%d %H:%M:%S")
+    COLS_TMP = ['Date', 'Close', 'STOCK', 'WALLET', 'PRINCIPAL', 'RATE', 'VALUATION']
+    RESULT_DF = pd.DataFrame(columns=COLS_TMP)
 
-            if date in DF.index:
-                # 주식 매수
-                STOCK = (BUDGET + WALLET) // ((DF.at[date, 'Close'])) + STOCK
-                WALLET = (BUDGET + WALLET) % ((DF.at[date, 'Close']))
-                PRINCIPAL += BUDGET
-                VALUATION = DF.at[date, 'Close'] * STOCK + WALLET
-                RATE = ((VALUATION / PRINCIPAL) - 1)
-                print('{}	{}	{}	{}	{}	{:.2}	{}'.format(
-                    str(date),
-                    DF.at[date, 'Close'],
-                    STOCK,
-                    WALLET,
-                    PRINCIPAL,
-                    RATE,
-                    VALUATION
-                    )
-                    )
-                break
-"""
+    for year in range(2010, 2021):
+        for month in range(1, 13):
+            for day in range(1, 28):
+                date = dt.datetime(year, month, day, 0, 0, 0)
+                date = date.__format__("%Y-%m-%d %H:%M:%S")
+
+                if date in DF.index:
+                    # 주식 매수
+                    STOCK = (BUDGET + WALLET) // ((DF.at[date, 'Close'])) + STOCK
+                    WALLET = (BUDGET + WALLET) % ((DF.at[date, 'Close']))
+                    PRINCIPAL += BUDGET
+                    VALUATION = DF.at[date, 'Close'] * STOCK + WALLET
+                    RATE = ((VALUATION / PRINCIPAL) - 1)
+                    date = dt.datetime(year, month, day)
+                    RESULT_DF = RESULT_DF.append({
+                        'Date':date,
+                        'PRINCIPAL':PRINCIPAL,
+                        'RATE':(RATE*100),
+                        'VALUATION':VALUATION},
+                        ignore_index=True)
+                    break
+        PRINCIPAL = PRINCIPAL + (PRINCIPAL * 0.017 * 0.846)
+    TITLE = pd.read_sql('select Symbol, Name from KRX where Symbol like \"' + SYM + '\"', con = CONN, index_col="Symbol")
+    print(TITLE.at[SYM, 'Name'])
+    print(RESULT_DF)
+    RESULT_DF = RESULT_DF.set_index('Date')
+    PLOT = RESULT_DF.plot(secondary_y=['RATE'], mark_right=False)
+    # ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    PLOT.set_title(SYM + ' ' + TITLE.at[SYM, 'Name'])
+    PLOT.right_ax.yaxis.set_major_formatter(mtick.PercentFormatter())
+    PLOT.ticklabel_format(axis='y', style='plain')
+
+    # 그래프 파일 저장
+    CSV_FIG = PNG_FOLDER + '%s.png' % (SYM)
+    FIG = PLOT.get_figure()
+    FIG.savefig(CSV_FIG)
+
+for SYM in sys.argv[1:]:
+    if (checkTableExists(CONN, SYM)):
+        calcValuation(SYM)
+    else:
+        print("Table is not exist, Create Table " + SYM)
+        # create_table(CONN, SYM)
+
 CONN.close()
 BT.close()
