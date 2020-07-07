4c69473d35fb6dd23bb387ad56fec2bae3b025de
JellyPo <jellypo@gmail.com>
2020-07-07 23:30:40 +0900

    db_sort.py 함수 도입, try except 추가

--- db_sort.py
+++ db_sort.py
@@ -5,14 +5,23 @@ import sqlite3
 CONN = sqlite3.connect('./db/finance.db')
 cur = CONN.cursor()
 
-DF = pd.read_sql("select Symbol from KRX", con=CONN)
+markets = [ "KRX", "NASDAQ", "NYSE", "AMEX", "SP500", "ETF_KR", "ETF_US" ]
 
-# DF.Symbol
-# 모든 종목코드 테이블 돌면서 날짜 중복된 레코드 삭제
-for Sym in DF.Symbol:
-    QUERY = 'delete from "' + Sym + '" where rowid not in ( select  min(rowid) from "' + Sym +'" group by Date )'
-    cur.execute(QUERY)
-    CONN.commit()
-    print(QUERY)
+def db_sort(CONN, Symbol):
+    for Sym in Symbol:
+        try:
+            QUERY = 'delete from "' + Sym + '" where rowid not in ( select  min(rowid) from "' + Sym +'" group by Date )'
+            print(QUERY)
+            cur.execute(QUERY)
+            CONN.commit()
+        except:
+            print('error')
+    
+for market in markets:
+    DF = pd.read_sql('select Symbol from \"' + market + '\"', con=CONN)
+    try:
+        db_sort(CONN, DF.Symbol)
+    except:
+        print(market + ' Error!')
 
 CONN.close()
