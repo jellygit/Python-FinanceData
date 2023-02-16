3a9f91693a35e101dd4de5746bc692a700b6d751
JellyPo <jellypo@gmail.com>
2023-02-16 09:37:28 +0900

    README.md: 재작성db_sort.py: 모든 DB 데이터 중복 제거하도록 재작성

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # Finance Data
 ## init_data.py
 사용 방법
-> python ./init_data.py
+> python ./init_data.py [MARKET]
+> # MARKET 은 FinanceDataReader 의 마켓 심볼 사용
+> # KRX NASDAQ NYSE AMEX ETF/KR ... 
 1. https://github.com/FinanceData/FinanceDataReader 모듈 사용
-1. 한국과 미국 종목코드 받아오기.
-1. 한국 1996년 부터 2019년 12월 30일까지 전종목 시가-종가-거래량 저장
- 1. ./db/finance.db 로 저장되며, 650 MB 가량 됨.
+1. 전종목 시가-종가-거래량 저장
  1. 종목 코드는 DB에 덮어씌우지만, 거래 데이터는 이어쓰기(Append)이기 때문에 중복 발생할 수 있음. 중복 발생시엔 db_sort.py 로 정리 할 것.
 
 ## stock_value.py
 사용 방법
 > python ./stock_value.py NNNNNN
-1. NNNNNN은 종목 코드, db/finance.db 에서 거래 데이터 받아와 매달 첫 거래일 종가에 매수 했을 때의 데이터를 출력함.
-1. 표출출력하게 되어 있음.
-1. 2020 년 까지 실행 시 에러가 남. -> 수정 완료, 날짜 중복 시 배열이 넘어와서 에러 발생한 것.
+1. 처음부터 뜯어고치는 중이라 사용 불가
 ### stock.ipynb
 stock_value.py 의 jupyter notebook 버전. sys.argv[1] 에 종목코드를 넣으면 인라인에 표과 그래프 출력함
 
@@ -52,3 +50,4 @@ CREATE TABLE IF NOT EXISTS "종목코드" (
 db/finance.db 파일에 중복된 거래 데이터가 생겼을 때, 날짜기준으로 중복 제거하는 스크립트
 > python ./db_sort.py
 * 그냥 실행하면 모든 종목코드 돌면서 중복 날짜 삭제.
+* 지원 거래소 별 정리하도록 수정할 예정(아직은 하드코딩으로 거래소 바꿔가며 삭제)
--- db_sort.py
+++ db_sort.py
@@ -1,10 +1,15 @@
 #!/bin/env python
+import os
+import sys
 import pandas as pd
 import sqlite3
 
-CONN = sqlite3.connect('./db/finance.db')
-cur = CONN.cursor()
+read_conn = sqlite3.connect('./db/finance.db')
+write_conn = sqlite3.connect('./db/backtest.db')
+fi_cur = read_conn.cursor()
+bt_cur = write_conn.cursor()
 
+<<<<<<< HEAD
 markets = [ "KRX", "NASDAQ", "NYSE", "AMEX", "SP500", "ETF_KR", "ETF_US" ]
 
 def db_sort(CONN, Symbol):
@@ -23,5 +28,64 @@ for market in markets:
         db_sort(CONN, DF.Symbol)
     except:
         print(market + ' Error!')
+=======
+## 인자 확인: 0는 실행파일 명, 1 이후가 인자
+## 비어 있으면 전체 마켓
+if len(sys.argv) > 1:
+    MARKETS = sys.argv
+    MARKETS.pop(0)
+else:
+    MARKETS = [ "ETF/KR", "KRX" ]
+    # MARKETS = [ "ETF/KR", "KRX", "NASDAQ", "NYSE", "SP500" ]
+
+SYMBOLS = pd.DataFrame()
+
+def getSymbols(MARKET):
+    df = pd.read_sql('select Symbol from \"' + MARKET + '"', con=read_conn)
+    return df
+
+def db_deduplication(Sym):
+    QUERY = 'delete from "' + Sym + '" where rowid not in ( select  min(rowid) from "' + Sym +'" group by Date )'
+    try:
+        fi_cur.execute(QUERY)
+        bt_cur.execute(QUERY)
+        read_conn.commit()
+        write_conn.commit()
+    except:
+        print("error")
+    print(QUERY)
+
+
+for MARKET in MARKETS:
+    append_df = getSymbols(MARKET)
+    SYMBOLS = pd.concat([SYMBOLS, append_df])
+
+    for Sym in SYMBOLS.Symbol:
+        db_deduplication(Sym)
+
+"""
+read_conn = sqlite3.connect('./db/finance.db')
+write_conn = sqlite3.connect('./db/backtest.db')
+cur = bt_CONN.cursor()
+
+DF = pd.read_sql("select Symbol from \"ETF/KR\"", con=CONN)
+#DF = pd.read_sql("select Symbol from KRX", con=CONN)
+
+# DF.Symbol
+# 모든 종목코드 테이블 돌면서 날짜 중복된 레코드 삭제
+for Sym in DF.Symbol:
+    QUERY = 'delete from "' + Sym + '" where rowid not in ( select  min(rowid) from "' + Sym +'" group by Date )'
+    try:
+        cur.execute(QUERY)
+        CONN.commit()
+    except:
+        print("error")
+    print(QUERY)
+>>>>>>> 4e60605 (README.md: 재작성db_sort.py: 모든 DB 데이터 중복 제거하도록 재작성)
 
 CONN.close()
+"""
+read_conn.close()
+write_conn.close()
+
+
