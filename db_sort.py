75564427d9276b5793a962f2c18762ca5b2116c4
JellyPo <jellypo@gmail.com>
2023-02-18 01:10:14 +0900

    name.py, bt.py 추가

--- db_sort.py
+++ db_sort.py
@@ -9,26 +9,6 @@ write_conn = sqlite3.connect('./db/backtest.db')
 fi_cur = read_conn.cursor()
 bt_cur = write_conn.cursor()
 
-<<<<<<< HEAD
-markets = [ "KRX", "NASDAQ", "NYSE", "AMEX", "SP500", "ETF_KR", "ETF_US" ]
-
-def db_sort(CONN, Symbol):
-    for Sym in Symbol:
-        try:
-            QUERY = 'delete from "' + Sym + '" where rowid not in ( select  min(rowid) from "' + Sym +'" group by Date )'
-            print(QUERY)
-            cur.execute(QUERY)
-            CONN.commit()
-        except:
-            print('error')
-    
-for market in markets:
-    DF = pd.read_sql('select Symbol from \"' + market + '\"', con=CONN)
-    try:
-        db_sort(CONN, DF.Symbol)
-    except:
-        print(market + ' Error!')
-=======
 ## 인자 확인: 0는 실행파일 명, 1 이후가 인자
 ## 비어 있으면 전체 마켓
 if len(sys.argv) > 1:
@@ -63,5 +43,27 @@ for MARKET in MARKETS:
     for Sym in SYMBOLS.Symbol:
         db_deduplication(Sym)
 
+"""
+read_conn = sqlite3.connect('./db/finance.db')
+write_conn = sqlite3.connect('./db/backtest.db')
+cur = bt_CONN.cursor()
+
+DF = pd.read_sql("select Symbol from \"ETF/KR\"", con=CONN)
+#DF = pd.read_sql("select Symbol from KRX", con=CONN)
+
+# DF.Symbol
+# 모든 종목코드 테이블 돌면서 날짜 중복된 레코드 삭제
+for Sym in DF.Symbol:
+    QUERY = 'delete from "' + Sym + '" where rowid not in ( select  min(rowid) from "' + Sym +'" group by Date )'
+    try:
+        cur.execute(QUERY)
+        CONN.commit()
+    except:
+        print("error")
+    print(QUERY)
+
+CONN.close()
+"""
 read_conn.close()
 write_conn.close()
+
--- name.py
+++ name.py
@@ -4,6 +4,19 @@ import sys
 import pandas as pd
 import sqlite3
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_seq_items', None)
+
+## 인자 확인: 0는 실행파일 명, 1 이후가 인자
+## 비어 있으면 전체 마켓 업데이트
+if len(sys.argv) > 1:
+    MARKETS = sys.argv
+    MARKETS.pop(0)
+else:
+    MARKETS = [ "KRX", "ETF/KR" ]
+    # MARKETS = [ "ETF/KR", "KRX", "NASDAQ", "NYSE", "SP500" ]
+
+
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
@@ -16,19 +29,14 @@ def create_connection(db_file):
         return conn
     except Error as e:
         print(e)
-
     return conn
 
-CONN = create_connection('./db/finance.db')
-BT = create_connection('./db/backtest.db')
+Symbol_name = pd.DataFrame()
+for MARKET in MARKETS:
+    CONN = create_connection('./db/finance.db')
+    df = pd.read_sql('select Symbol, Name from \"'+ MARKET +'"', con = CONN)
+    Symbol_name = pd.concat([Symbol_name, df])
 
-file=open('list.txt', 'r')
-ALL=file.read()
-lines = ALL.split('\n')
-for line in lines:
-    DF = pd.read_sql('select Symbol, Name from KRX where Symbol like \"' + line +'\" union select Symbol, Name from ETF_KR where Symbol like \"' + line + '\"', con = CONN)
-    print (DF)
-    # print (line)
+print(Symbol_name)
 
 CONN.close()
-BT.close()
